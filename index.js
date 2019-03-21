3772b3fc6a87c85e701672cf93856abe0662abe6
Elliot svensen <elliotsvensen@hotmail.co.uk>
2019-03-21 11:12:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ item = {itemName: nameOfItem, itemPrice: priceOfItem};
+ cart.push(item);
 }
 
 function viewCart() {
